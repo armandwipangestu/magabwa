be51e5de4a6ed8e5c70af15db4d10032b42be2e4
Arman <armandwi.pangestu7@gmail.com>
2024-08-17 18:38:32 +0700

    frontend: show featured news and ads

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\ArticleNews;
 use App\Models\Author;
+use App\Models\BannerAdvertisment;
 use App\Models\Category;
 use Illuminate\Http\Request;
 
@@ -17,7 +18,17 @@ class FrontController extends Controller
             ->latest()
             ->take(3)
             ->get();
+        $featured_articles = ArticleNews::with(['category'])
+            ->where('is_featured', 'featured')
+            ->inRandomOrder()
+            ->take(3)
+            ->get();
         $authors = Author::all();
-        return view('front.index', compact('categories', 'articles', 'authors'));
+        $banner_ads = BannerAdvertisment::where('is_active', 'active')
+            ->where('type', 'banner')
+            ->inRandomOrder()
+            // ->take(1)
+            ->first();
+        return view('front.index', compact('categories', 'articles', 'authors', 'featured_articles', 'banner_ads'));
     }
 }
--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -45,63 +45,29 @@
 		</nav>
 		<section id="Featured" class="mt-[30px]">
 			<div class="main-carousel w-full">
-				<div class="featured-news-card relative w-full h-[550px] flex shrink-0 overflow-hidden">
-					<img src="assets/images/thumbnails/th-featured-1.png" class="thumbnail absolute w-full h-full object-cover" alt="icon" />
-					<div class="w-full h-full bg-gradient-to-b from-[rgba(0,0,0,0)] to-[rgba(0,0,0,0.9)] absolute z-10"></div>
-					<div class="card-detail max-w-[1130px] w-full mx-auto flex items-end justify-between pb-10 relative z-20">
-						<div class="flex flex-col gap-[10px]">
-							<p class="text-white">Featured</p>
-							<a href="details.html" class="font-bold text-4xl leading-[45px] text-white two-lines hover:underline transition-all duration-300">Generation Z Prefer Remote Working Than Big Salary</a>
-							<p class="text-white">12 Jun, 2024 • Business</p>
-						</div>
-						<div class="prevNextButtons flex items-center gap-4 mb-[60px]">
-							<button class="button--previous appearance-none w-[38px] h-[38px] flex items-center justify-center rounded-full shrink-0 ring-1 ring-white hover:ring-2 hover:ring-[#FF6B18] transition-all duration-300">
-								<img src="assets/images/icons/arrow.svg" alt="arrow" />
-							</button>
-							<button class="button--next appearance-none w-[38px] h-[38px] flex items-center justify-center rounded-full shrink-0 ring-1 ring-white hover:ring-2 hover:ring-[#FF6B18] transition-all duration-300 rotate-180">
-								<img src="assets/images/icons/arrow.svg" alt="arrow" />
-							</button>
-						</div>
-					</div>
-				</div>
-				<div class="featured-news-card relative w-full h-[550px] flex shrink-0 overflow-hidden">
-					<img src="assets/images/thumbnails/th-featured-1.png" class="thumbnail absolute w-full h-full object-cover" alt="icon" />
-					<div class="w-full h-full bg-gradient-to-b from-[rgba(0,0,0,0)] to-[rgba(0,0,0,0.9)] absolute z-10"></div>
-					<div class="card-detail max-w-[1130px] w-full mx-auto flex items-end justify-between pb-10 relative z-20">
-						<div class="flex flex-col gap-[10px]">
-							<p class="text-white">Featured</p>
-							<a href="details.html" class="font-bold text-4xl leading-[45px] text-white two-lines hover:underline transition-all duration-300">Generation Z Prefer Remote Working Than Big Salary</a>
-							<p class="text-white">12 Jun, 2024 • Business</p>
-						</div>
-						<div class="prevNextButtons flex items-center gap-4 mb-[60px]">
-							<button class="button--previous appearance-none w-[38px] h-[38px] flex items-center justify-center rounded-full shrink-0 ring-1 ring-white hover:ring-2 hover:ring-[#FF6B18] transition-all duration-300">
-								<img src="assets/images/icons/arrow.svg" alt="arrow" />
-							</button>
-							<button class="button--next appearance-none w-[38px] h-[38px] flex items-center justify-center rounded-full shrink-0 ring-1 ring-white hover:ring-2 hover:ring-[#FF6B18] transition-all duration-300 rotate-180">
-								<img src="assets/images/icons/arrow.svg" alt="arrow" />
-							</button>
-						</div>
-					</div>
-				</div>
-				<div class="featured-news-card relative w-full h-[550px] flex shrink-0 overflow-hidden">
-					<img src="assets/images/thumbnails/th-featured-1.png" class="thumbnail absolute w-full h-full object-cover" alt="icon" />
-					<div class="w-full h-full bg-gradient-to-b from-[rgba(0,0,0,0)] to-[rgba(0,0,0,0.9)] absolute z-10"></div>
-					<div class="card-detail max-w-[1130px] w-full mx-auto flex items-end justify-between pb-10 relative z-20">
-						<div class="flex flex-col gap-[10px]">
-							<p class="text-white">Featured</p>
-							<a href="details.html" class="font-bold text-4xl leading-[45px] text-white two-lines hover:underline transition-all duration-300">Generation Z Prefer Remote Working Than Big Salary</a>
-							<p class="text-white">12 Jun, 2024 • Business</p>
-						</div>
-						<div class="prevNextButtons flex items-center gap-4 mb-[60px]">
-							<button class="button--previous appearance-none w-[38px] h-[38px] flex items-center justify-center rounded-full shrink-0 ring-1 ring-white hover:ring-2 hover:ring-[#FF6B18] transition-all duration-300">
-								<img src="assets/images/icons/arrow.svg" alt="arrow" />
-							</button>
-							<button class="button--next appearance-none w-[38px] h-[38px] flex items-center justify-center rounded-full shrink-0 ring-1 ring-white hover:ring-2 hover:ring-[#FF6B18] transition-all duration-300 rotate-180">
-								<img src="assets/images/icons/arrow.svg" alt="arrow" />
-							</button>
-						</div>
-					</div>
-				</div>
+				@forelse ($featured_articles as $article)
+                    <div class="featured-news-card relative w-full h-[550px] flex shrink-0 overflow-hidden">
+                        <img src="{{ Storage::url($article->thumbnail) }}" class="thumbnail absolute w-full h-full object-cover" alt="icon" />
+                        <div class="w-full h-full bg-gradient-to-b from-[rgba(0,0,0,0)] to-[rgba(0,0,0,0.9)] absolute z-10"></div>
+                        <div class="card-detail max-w-[1130px] w-full mx-auto flex items-end justify-between pb-10 relative z-20">
+                            <div class="flex flex-col gap-[10px]">
+                                <p class="text-white">Featured</p>
+                                <a href="{{ route('front.details', $article->slug) }}" class="font-bold text-4xl leading-[45px] text-white two-lines hover:underline transition-all duration-300">{{ $article->name }}</a>
+                                <p class="text-white">{{ $article->created_at->format('M d, Y') }} • {{ $article->category->name }}</p>
+                            </div>
+                            <div class="prevNextButtons flex items-center gap-4 mb-[60px]">
+                                <button class="button--previous appearance-none w-[38px] h-[38px] flex items-center justify-center rounded-full shrink-0 ring-1 ring-white hover:ring-2 hover:ring-[#FF6B18] transition-all duration-300">
+                                    <img src="assets/images/icons/arrow.svg" alt="arrow" />
+                                </button>
+                                <button class="button--next appearance-none w-[38px] h-[38px] flex items-center justify-center rounded-full shrink-0 ring-1 ring-white hover:ring-2 hover:ring-[#FF6B18] transition-all duration-300 rotate-180">
+                                    <img src="assets/images/icons/arrow.svg" alt="arrow" />
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                @empty
+                    <p>Belum ada data terbaru</p>
+                @endforelse
 			</div>
 		</section>
 		<section id="Up-to-date" class="max-w-[1130px] mx-auto flex flex-col gap-[30px] mt-[70px]">
@@ -159,9 +125,9 @@
 		</section>
 		<section id="Advertisement" class="max-w-[1130px] mx-auto flex justify-center mt-[70px]">
 			<div class="flex flex-col gap-3 shrink-0 w-fit">
-				<a href="#">
+				<a href="{{ $banner_ads->link }}">
 					<div class="w-[900px] h-[120px] flex shrink-0 border border-[#EEF0F7] rounded-2xl overflow-hidden">
-						<img src="assets/images/iklans/bannerWide1.png" class="object-cover w-full h-full" alt="ads" />
+						<img src="{{ Storage::url($banner_ads->thumbnail) }}" class="object-cover w-full h-full" alt="ads" />
 					</div>
 				</a>
 				<p class="font-medium text-sm leading-[21px] text-[#A3A6AE] flex gap-1">
