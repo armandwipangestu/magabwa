d40d27104f1df65985db4d74dd08e489f613342b
Arman <armandwi.pangestu7@gmail.com>
2024-08-17 18:03:57 +0700

    frontend: prepare route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\FrontController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [FrontController::class, 'index'])->name('front.index');
+Route::get('/details/{article_news:slug}', [FrontController::class, 'details'])->name('front.details');
+Route::get('/category/{category:slug}', [FrontController::class, 'category'])->name('front.category');
+Route::get('/author/{author:slug}', [FrontController::class, 'author'])->name('front.author');
+Route::get('/search', [FrontController::class, 'search'])->name('front.search');
