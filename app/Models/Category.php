62f77def505c61bca7ba2b3cba408312f8a9f18b
Arman <armandwi.pangestu7@gmail.com>
2024-08-17 12:05:01 +0700

    filament: CRUD Data Category

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Str;
 
 class Category extends Model
 {
@@ -16,6 +17,13 @@ class Category extends Model
         'slug',
         'icon'
     ];
+
+    // Laravel Accessor
+    public function setNameAttribute($value)
+    {
+        $this->attributes['name'] = $value;
+        $this->attributes['slug'] = Str::slug($value);
+    }
 
     public function news(): HasMany
     {
